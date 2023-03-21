eb6e76893805f04cb71f1051f31217319e20e6f1
Chris Mixa <91347624+CMIXA@users.noreply.github.com>
2023-03-21 01:14:01 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">
+        <img
+          src="logo.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="Immersive Insights logo"
+        />
+        Immersive Insights
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <NavDropdown title="Services" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#unmoderated-testing">
+              Unmoderated Testing
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#moderated-testing">
+              Moderated Testing
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#user-interviews">
+              User Interviews
+            </NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Resources" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#guides">Guides</NavDropdown.Item>
+            <NavDropdown.Item href="#tools">Tools</NavDropdown.Item>
+            <NavDropdown.Item href="#glossary">Glossary</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#about-us">About Us</Nav.Link>
+          <Nav.Link href="#contact-us">Contact Us</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#login">Login</Nav.Link>
+          <Button variant="primary">Get Paid to Test</Button>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default App;
