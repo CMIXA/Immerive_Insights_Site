b3112d276cf8caaf87f041d69a69f0837a093666
Chris Mixa <91347624+CMIXA@users.noreply.github.com>
2023-03-22 02:25:54 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
+import "./App.css";
+import logo from "./images/logo.png";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar />
+    // <Navbar className="Navbar">
+    //   <Navbar.Container className="Navbar"></Navbar.Container>
+    //   <Navbar.Brand className="Navbar-Brand" href="#home">
+    //     <img
+    //       src={logo}
+    //       width="30"
+    //       height="30"
+    //       className="d-inline-block align-top"
+    //       alt="Immersive Insights logo"
+    //     />
+    //     Immersive Insights
+    //   </Navbar.Brand>
+    //   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //   <Navbar.Collapse id="basic-navbar-nav">
+    //     <Nav className="mr-auto">
+    //       <NavDropdown title="Services" id="basic-nav-dropdown">
+    //         <NavDropdown.Item href="#unmoderated-testing">
+    //           Unmoderated Testing
+    //         </NavDropdown.Item>
+    //         <NavDropdown.Item href="#moderated-testing">
+    //           Moderated Testing
+    //         </NavDropdown.Item>
+    //         <NavDropdown.Item href="#user-interviews">
+    //           User Interviews
+    //         </NavDropdown.Item>
+    //       </NavDropdown>
+    //       <NavDropdown title="Resources" id="basic-nav-dropdown">
+    //         <NavDropdown.Item href="#guides">Guides</NavDropdown.Item>
+    //         <NavDropdown.Item href="#tools">Tools</NavDropdown.Item>
+    //         <NavDropdown.Item href="#glossary">Glossary</NavDropdown.Item>
+    //       </NavDropdown>
+    //       <Nav.Link href="#pricing">Pricing</Nav.Link>
+    //       <Nav.Link href="#about-us">About Us</Nav.Link>
+    //       <Nav.Link href="#contact-us">Contact Us</Nav.Link>
+    //     </Nav>
+    //     <Nav>
+    //       <Nav.Link href="#login">Login</Nav.Link>
+    //       <Button variant="primary">Get Paid to Test</Button>
+    //     </Nav>
+    //   </Navbar.Collapse>
+    // </Navbar>
   );
 }
 
