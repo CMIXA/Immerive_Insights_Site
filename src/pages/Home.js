98023a849c95434387f8bba449317c4af0370470
Chris Mixa <91347624+CMIXA@users.noreply.github.com>
2023-03-30 01:52:45 -0400

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,23 @@ const Home = () => {
       <div className="h-section-1"></div>
       <div className="h-section-2"></div>
       <div className="h-section-3"></div>
-      <div className="h-section-4"></div>
-      <div className="h-section-5"></div>
+      <div className="h-section-4">
+        <div className="h-s4-wrapper">
+          <div className="h-s4-title">
+            <h2>The Testing Process</h2>
+            <img />
+          </div>
+        </div>
+      </div>
+      <div className="h-section-5">
+        <div className="h-s5-wrapper">
+          <div className="h-s5-title">
+            <h2>The best creators trust Immersive Insights</h2>
+            <img />
+          </div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
